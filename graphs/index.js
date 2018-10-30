53a14c352ac5a056d75d96cd2c807846873358e1
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-10-30 10:28:46 -0700

    begin implementation of graph

--- graphs/index.js
+++ graphs/index.js
@@ -1,5 +1,28 @@
 function createGraph() {
-  return {}
+  const graph = new Map()
+
+  return {
+    get nodes() {
+      return Array.from(graph.keys())
+    },
+    getNode(node) {
+      return graph.get(node)
+    },
+    addNode(node) {
+      graph.set(node, [])
+    },
+    addEdge(node1, node2) {
+      if (graph.has(node1) && graph.has(node2)) {
+        graph.get(node1).push(node2)
+        graph.get(node2).push(node1)
+      }
+    },
+    print() {
+      return Array.from(graph.entries())
+        .map(([key, values]) => `${key} -> ${values.join(' ')}`)
+        .join('\n')
+    }
+  }
 }
 
 exports.createGraph = createGraph
--- graphs/index.test.js
+++ graphs/index.test.js
@@ -5,4 +5,51 @@ describe('Graph', () => {
   beforeEach(() => {
     graph = createGraph()
   })
+
+  test('addNode', () => {
+    expect(graph.nodes.includes('A')).toEqual(false)
+    graph.addNode('A')
+    expect(graph.nodes.includes('A')).toEqual(true)
+  })
+
+  test('addEdge', () => {
+    graph.addNode('a')
+    graph.addNode('b')
+    graph.addEdge('a', 'b')
+
+    expect(graph.getNode('a').includes('b')).toEqual(true)
+    expect(graph.getNode('b').includes('a')).toEqual(true)
+  })
+
+  test('getNode', () => {
+    graph.addNode('a')
+    graph.addNode('b')
+    graph.addNode('c')
+    graph.addEdge('a', 'b')
+    graph.addEdge('a', 'c')
+
+    const nodeA = graph.getNode('a')
+    expect(nodeA.includes('b')).toEqual(true)
+    expect(nodeA.includes('c')).toEqual(true)
+  })
+
+  test('print', () => {
+    const nodes = ['a', 'b', 'c', 'd', 'e']
+    nodes.forEach(node => {
+      graph.addNode(node)
+    })
+    graph.addEdge('a', 'b')
+    graph.addEdge('a', 'c')
+    graph.addEdge('a', 'e')
+    graph.addEdge('b', 'c')
+    graph.addEdge('b', 'd')
+
+    expect(graph.print()).toEqual(
+      `a -> b c e
+b -> a c d
+c -> a b
+d -> b
+e -> a`
+    )
+  })
 })
