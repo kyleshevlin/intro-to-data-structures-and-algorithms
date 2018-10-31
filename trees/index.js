31123e8481904bba44a553a617c1c1fd3b111d83
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-10-31 11:26:52 -0700

    implement trees and binary trees

--- trees/index.js
+++ trees/index.js
@@ -1,5 +1,107 @@
-function createTree() {
-  return {}
+function createNode(key) {
+  const children = []
+
+  return {
+    key,
+    children,
+    addChild(childKey) {
+      const childNode = createNode(childKey)
+      children.push(childNode)
+      return childNode
+    }
+  }
 }
 
+function createBinaryNode(key) {
+  return {
+    key,
+    left: null,
+    right: null,
+    addLeft(leftKey) {
+      const newLeft = createBinaryNode(leftKey)
+      this.left = newLeft
+      return newLeft
+    },
+    addRight(rightKey) {
+      const newRight = createBinaryNode(rightKey)
+      this.right = newRight
+      return newRight
+    }
+  }
+}
+
+function createTree(rootKey) {
+  const root = createNode(rootKey)
+
+  return {
+    root,
+    print() {
+      let result = root.key
+
+      function traverse(node, visitFn) {
+        if (node.children.length) {
+          node.children.forEach(visitFn)
+          node.children.forEach(n => traverse(n, visitFn))
+        }
+      }
+
+      function addKeyToResult(node) {
+        result += ` => ${node.key}`
+      }
+
+      traverse(root, addKeyToResult)
+
+      return result
+    }
+  }
+}
+
+function createBinaryTree(rootKey) {
+  const root = createBinaryNode(rootKey)
+
+  const TRAVERSALS = {
+    IN_ORDER: (node, visitFn) => {
+      if (node !== null) {
+        TRAVERSALS.IN_ORDER(node.left, visitFn)
+        visitFn(node)
+        TRAVERSALS.IN_ORDER(node.right, visitFn)
+      }
+    },
+
+    PRE_ORDER: (node, visitFn) => {
+      if (node !== null) {
+        visitFn(node)
+        TRAVERSALS.PRE_ORDER(node.left, visitFn)
+        TRAVERSALS.PRE_ORDER(node.right, visitFn)
+      }
+    },
+
+    POST_ORDER: (node, visitFn) => {
+      if (node !== null) {
+        TRAVERSALS.POST_ORDER(node.left, visitFn)
+        TRAVERSALS.POST_ORDER(node.right, visitFn)
+        visitFn(node)
+      }
+    }
+  }
+
+  return {
+    root,
+    print(orderType = 'IN_ORDER') {
+      let result = ''
+
+      const visitFn = node => {
+        result += result.length === 0 ? node.key : ` => ${node.key}`
+      }
+
+      TRAVERSALS[orderType](this.root, visitFn)
+
+      return result
+    }
+  }
+}
+
+exports.createBinaryNode = createBinaryNode
+exports.createBinaryTree = createBinaryTree
+exports.createNode = createNode
 exports.createTree = createTree
--- trees/index.test.js
+++ trees/index.test.js
@@ -1,12 +1,156 @@
-const { createTree } = require('./index')
+const {
+  createBinaryNode,
+  createBinaryTree,
+  createTree,
+  createNode
+} = require('./index')
+
+describe('Node', () => {
+  let node
+  beforeEach(() => {
+    node = createNode('a')
+  })
+
+  test('existence', () => {
+    expect(node).toBeDefined()
+  })
+
+  test('key', () => {
+    expect(node.key).toEqual('a')
+  })
+
+  test('children', () => {
+    expect(node.children).toBeDefined()
+    expect(node.children.length).toEqual(0)
+  })
+
+  test('addChild', () => {
+    node.addChild('b')
+
+    expect(node.children.length).toEqual(1)
+    expect(node.children.map(n => n.key).includes('b')).toBe(true)
+  })
+})
+
+describe('BinaryNode', () => {
+  let node
+  beforeEach(() => {
+    node = createBinaryNode('a')
+  })
+
+  test('existence', () => {
+    expect(node).toBeDefined()
+  })
+
+  test('key', () => {
+    expect(node.key).toEqual('a')
+  })
+
+  test('left', () => {
+    expect(node.left).toBeNull()
+  })
+
+  test('right', () => {
+    expect(node.right).toBeNull()
+  })
+
+  test('addLeft', () => {
+    node.addLeft('b')
+
+    expect(node.left).toBeDefined()
+    expect(node.left.key).toEqual('b')
+  })
+
+  test('addRight', () => {
+    node.addRight('c')
+
+    expect(node.right).toBeDefined()
+    expect(node.right.key).toEqual('c')
+  })
+})
 
 describe('Tree', () => {
   let tree
   beforeEach(() => {
-    tree = createTree()
+    tree = createTree('a')
   })
 
-  test('appeaseJestForNow', () => {
-    expect(true).toBe(true)
+  test('existence', () => {
+    expect(tree).toBeDefined()
+  })
+
+  test('root', () => {
+    expect(tree.root).toBeDefined()
+    expect(tree.root.key).toEqual('a')
+  })
+
+  test('print', () => {
+    const b = tree.root.addChild('b')
+    const c = tree.root.addChild('c')
+    const d = b.addChild('d')
+    const e = b.addChild('e')
+    const f = c.addChild('f')
+    const g = c.addChild('g')
+    const h = f.addChild('h')
+
+    expect(tree.print()).toEqual(`a => b => c => d => e => f => g => h`)
+  })
+})
+
+describe('BinaryTree', () => {
+  let binaryTree
+  beforeEach(() => {
+    binaryTree = createBinaryTree('a')
+  })
+
+  test('existence', () => {
+    expect(binaryTree).toBeDefined()
+  })
+
+  test('root', () => {
+    expect(binaryTree.root).toBeDefined()
+    expect(binaryTree.root.key).toEqual('a')
+  })
+
+  describe('print', () => {
+    test('IN_ORDER', () => {
+      const b = binaryTree.root.addLeft('b')
+      const c = binaryTree.root.addRight('c')
+      const d = b.addLeft('d')
+      const e = b.addRight('e')
+      const f = c.addLeft('f')
+      const g = c.addRight('g')
+      const h = f.addLeft('h')
+
+      expect(binaryTree.print()).toEqual(`d => b => e => a => h => f => c => g`)
+    })
+
+    test('PRE_ORDER', () => {
+      const b = binaryTree.root.addLeft('b')
+      const c = binaryTree.root.addRight('c')
+      const d = b.addLeft('d')
+      const e = b.addRight('e')
+      const f = c.addLeft('f')
+      const g = c.addRight('g')
+      const h = f.addLeft('h')
+
+      expect(binaryTree.print('PRE_ORDER')).toEqual(
+        `a => b => d => e => c => f => h => g`
+      )
+    })
+
+    test('POST_ORDER', () => {
+      const b = binaryTree.root.addLeft('b')
+      const c = binaryTree.root.addRight('c')
+      const d = b.addLeft('d')
+      const e = b.addRight('e')
+      const f = c.addLeft('f')
+      const g = c.addRight('g')
+      const h = f.addLeft('h')
+
+      expect(binaryTree.print('POST_ORDER')).toEqual(
+        `d => e => b => h => f => g => c => a`
+      )
+    })
   })
 })
