21d32f84c408a869d8f259ea894eab3fb510c3bb
Zac Jones <zacjones93@gmail.com>
2018-11-28 22:14:07 -0500

    adds tree lesson

---
+++ trees/tree.js
@@ -0,0 +1,122 @@
+function createNode(key) {
+  const children = []
+
+  return {
+    key,
+    children,
+    addChild(childKey) {
+      const childNode = createNode(childKey)
+      children.push(childNode)
+      return childNode
+    }
+  }
+}
+
+function createBinaryNode(key) {
+  return {
+    key,
+    left: null,
+    right: null,
+    addLeft(leftKey) {
+      const newLeft = createBinaryNode(leftKey)
+      this.left = newLeft
+      return newLeft
+    },
+    addRight(rightKey) {
+      const newRight = createBinaryNode(rightKey)
+      this.right = newRight
+      return newRight
+    }
+  }
+}
+
+function createTree(rootKey) {
+  const root = createNode(rootKey)
+
+  return {
+    root,
+    print() {
+      let result = root.key
+
+      function traverse(node, visitFn) {
+        if (node.children.length) {
+          node.children.forEach(visitFn)
+          node.children.forEach(n => traverse(n, visitFn))
+        }
+      }
+
+      function addKeyToResult(node) {
+        result += ` => ${node.key}`
+      }
+
+      traverse(root, addKeyToResult)
+
+      return result
+    }
+  }
+}
+
+function createBinaryTree(rootKey) {
+  const root = createBinaryNode(rootKey)
+
+  const TRAVERSALS = {
+    IN_ORDER: (node, visitFn) => {
+      if (node !== null) {
+        TRAVERSALS.IN_ORDER(node.left, visitFn)
+        visitFn(node)
+        TRAVERSALS.IN_ORDER(node.right, visitFn)
+      }
+    },
+
+    PRE_ORDER: (node, visitFn) => {
+      if (node !== null) {
+        visitFn(node)
+        TRAVERSALS.PRE_ORDER(node.left, visitFn)
+        TRAVERSALS.PRE_ORDER(node.right, visitFn)
+      }
+    },
+
+    POST_ORDER: (node, visitFn) => {
+      if (node !== null) {
+        TRAVERSALS.POST_ORDER(node.left, visitFn)
+        TRAVERSALS.POST_ORDER(node.right, visitFn)
+        visitFn(node)
+      }
+    }
+  }
+
+  return {
+    root,
+    print(orderType = 'IN_ORDER') {
+      let result = ''
+
+      const visitFn = node => {
+        result += result.length === 0 ? node.key : ` => ${node.key}`
+      }
+
+      TRAVERSALS[orderType](this.root, visitFn)
+
+      return result
+    }
+  }
+}
+
+const dom = createTree('html')
+const head = dom.root.addChild('head')
+const body = dom.root.addChild('body')
+const title = head.addChild(
+  'title - egghead Tree Lesson'
+)
+const head = dom.body.addChild('header')
+const main = dom.body.addChild('main')
+const footer = dom.body.addChild('footer')
+const h1 = dom.header.addChild('h1 - Tree Lesson')
+const p = dom.main.addChild('p - Learn about trees!')
+const copyright = footer.addChild(`Copyright ${new Date().getFullYear()}`)
+
+console.log(dom.print())
+
+exports.createBinaryNode = createBinaryNode
+exports.createBinaryTree = createBinaryTree
+exports.createNode = createNode
+exports.createTree = createTree
