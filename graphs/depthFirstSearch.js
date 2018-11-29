810a831854dbb76541931d298e141993922414f6
Zac Jones <zacjones93@gmail.com>
2018-11-28 21:35:18 -0500

    adds basic set up for graph

---
+++ graphs/depthFirstSearch.js
@@ -0,0 +1,143 @@
+const { createQueue } = require('../queues')
+
+function createNode(key) {
+  const children = []
+
+  return {
+    key,
+    children,
+    addChild(node) {
+      children.push(node)
+    }
+  }
+}
+
+function createGraph(directed = false) {
+  const nodes = []
+  const edges = []
+
+  return {
+    directed,
+    nodes,
+    edges,
+
+    addNode(key) {
+      nodes.push(createNode(key))
+    },
+
+    getNode(key) {
+      return nodes.find(n => n.key === key)
+    },
+
+    addEdge(node1Key, node2Key) {
+      const node1 = this.getNode(node1Key)
+      const node2 = this.getNode(node2Key)
+
+      node1.addChild(node2)
+
+      if (!directed) {
+        node2.addChild(node1)
+      }
+
+      edges.push(`${node1Key}${node2Key}`)
+    },
+
+    print() {
+      return nodes
+        .map(({ children, key }) => {
+          let result = `${key}`
+
+          if (children.length) {
+            result += ` => ${children
+              .map(node => node.key)
+              .join(' ')}`
+          }
+
+          return result
+        })
+        .join('\n')
+    },
+
+    bfs(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(
+        startingNodeKey
+      )
+      const visitedHash = nodes.reduce(
+        (acc, cur) => {
+          acc[cur.key] = false
+          return acc
+        },
+        {}
+      )
+      const queue = createQueue()
+      queue.enqueue(startingNode)
+
+      while (!queue.isEmpty()) {
+        const currentNode = queue.dequeue()
+
+        if (!visitedHash[currentNode.key]) {
+          visitFn(currentNode)
+          visitedHash[currentNode.key] = true
+        }
+
+        currentNode.children.forEach(node => {
+          if (!visitedHash[node.key]) {
+            queue.enqueue(node)
+          }
+        })
+      }
+    },
+
+    dfs(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(
+        startingNodeKey
+      )
+      const visitedHash = nodes.reduce(
+        (acc, cur) => {
+          acc[cur.key] = false
+          return acc
+        },
+        {}
+      )
+
+      function explore(node) {
+        if (visitedHash[node.key]) {
+          return
+        }
+
+        visitFn(node)
+        visitedHash[node.key] = true
+
+        node.children.forEach(child => {
+          explore(child)
+        })
+      }
+
+      explore(startingNode)
+    }
+  }
+}
+
+const graph = createGraph(true)
+const nodes = ['a', 'b', 'c', 'd', 'e', 'f']
+const edges = [
+  ['a', 'b'],
+  ['a', 'e'],
+  ['a', 'f'],
+  ['b', 'd'],
+  ['b', 'e'],
+  ['c', 'b'],
+  ['d', 'c'],
+  ['d', 'e'],
+]
+
+nodes.forEach(node => {
+  graph.addNode(node)
+})
+
+edges.forEach((...nodes) => {
+  graph.addEdge(...nodes)
+})
+
+exports.createNode = createNode
+exports.createGraph = createGraph
--- priorityQueue/priorityQueue.js
+++ priorityQueue/priorityQueue.js
@@ -32,4 +32,15 @@ function createPriorityQueue() {
   }
 }
 
+const q = createPriorityQueue()
+
+q.enqueue('A fix here')
+q.enqueue('A bug there')
+q.enqueue('A new feature')
+
+q.dequeue()
+q.enqueue('Emergency task!', true)
+console.log(q.dequeue())
+console.log(q.peek())
+
 exports.createPriorityQueue = createPriorityQueue
